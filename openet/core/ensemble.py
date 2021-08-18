162c30143f7b15fd7e9c2d821ba3848853ecf618
Charles Morton <charles.g.morton@gmail.com>
2021-08-18 15:42:23 -0700

    Updated ensemble mad tests and removed crop_mask dependency

--- openet/core/ensemble.py
+++ openet/core/ensemble.py
@@ -1,13 +1,15 @@
 import ee
 
+from . import utils
+# import openet.core.utils as utils
 
-def median_absolute_deviation(images, crop_mask, made_scale=2):
+
+def median_absolute_deviation(images, made_scale=2):
     """Median absolute deviation
 
     Parameters
     ----------
     images : list
-    crop_mask : ee.Image
     made_scale : float, optional
 
     Returns
@@ -15,52 +17,46 @@ def median_absolute_deviation(images, crop_mask, made_scale=2):
     ee.Image
 
     """
-    # TODO: This should eventually change based on the image list size
-    model_count = 6
+    # TODO: Add checks on model count and minimum model count
+    model_count = len(images)
+    # model_count = 6
+    min_model_count = 4
 
-    # CGM - For now assume SIMS has already been masked to crop areas
-    # Compute a SIMS image for just crop pixels (mask out non agg for sims)
-    # sims_crop = sims.updateMask(crop_mask)
-
-    # Compute the Ensemble for all models and for SIMS only in crop pixels
-    # ensemble_sims_crop = ee.Image([disalexi, eemetric, geesebal, ptjpl, sims_crop, ssebop])
-
-    ensemble_sims_crop = ee.Image(images)
-
-    # Compute simple mean, median, and MADe
-    # ens_mean = ensemble_sims_crop.reduce(ee.Reducer.mean())\
-    #     .rename(["mean"])
-    ens_median = ensemble_sims_crop.reduce(ee.Reducer.median())\
-        .rename(["median"])
-    MADe = ensemble_sims_crop.subtract(ens_median).abs()\
-        .reduce(ee.Reducer.median())\
-        .multiply(1.4826).rename(["MADe"])
+    ensemble_img = ee.Image(images)
+    ens_median = ensemble_img.reduce(ee.Reducer.median()).rename(["median"])
+    MADe = ensemble_img.subtract(ens_median).abs()\
+        .reduce(ee.Reducer.median()).multiply(1.4826)
 
     # Calulate upper and lower bounds for outlier, based on median
     upper = ens_median.add(MADe.multiply(made_scale)).rename(["upper"])
     lower = ens_median.subtract(MADe.multiply(made_scale)).rename(["lower"])
 
-    # Drop Counts
-    count_sims = ensemble_sims_crop.lt(lower).add(ensemble_sims_crop.gt(upper))\
-        .reduce(ee.Reducer.sum()).add(crop_mask.Not()).rename(["count"])
-    count_img = ee.Image(model_count).subtract(count_sims).clamp(4, model_count)
+    # Count of ensemble values after applying MAD filtering
+    count_img = ensemble_img.gte(lower).And(ensemble_img.lte(upper))\
+        .reduce(ee.Reducer.sum()).clamp(min_model_count, model_count).rename(["count"])
 
-    ens = ensemble_sims_crop.unmask(-9999).toArray()
-    diff = ens.subtract(ens_median)
-    # TODO: Is the first .toArray needed?  Isn't ens already an array?
-    ens_sort = ens.toArray().arraySort(diff.abs().toArray())
+    ens_array = ensemble_img.unmask(-9999).toArray()
+    diff_array = ens_array.subtract(ens_median)
+    ens_sort = ens_array.arraySort(diff_array.abs())
 
-    array_null = ee.Image.constant([-9999] * model_count)\
-        .toArray()
-
+    array_null = ee.Image.constant([-9999] * model_count).toArray()
     sort_img = ens_sort.arraySlice(0, 0, count_img).arrayCat(array_null, 0)\
-        .arraySlice(0, 0, model_count)\
-        .arrayFlatten([[f'B{b+1}' for b in range(model_count)]])
+        .arraySlice(0, 0, 6).arrayFlatten([[f'B{b+1}' for b in range(model_count)]])
 
     model_drop_mean = sort_img.mask(sort_img.neq(-9999))\
-        .reduce(ee.Reducer.mean())
+        .reduce(ee.Reducer.mean()).rename(["ensemble"])
 
-    return model_drop_mean.rename(["ensemble"])
+    # DEADBEEF
+    # print(utils.point_image_value(ee.Image(images), [-120, 39], scale=1))
+    # print(utils.point_image_value(ens_median, [-120, 39], scale=1))
+    # print(utils.point_image_value(MADe, [-120, 39], scale=1))
+    # print(utils.point_image_value(upper, [-120, 39], scale=1))
+    # print(utils.point_image_value(lower, [-120, 39], scale=1))
+    # print(utils.point_image_value(count_img, [-120, 39], scale=1))
+    # print(utils.point_image_value(sort_img, [-120, 39], scale=1))
+    # print(utils.point_image_value(model_drop_mean, [-120, 39], scale=1))
+
+    return model_drop_mean
 
     # # CGM - Commented out returning model indices for now
     # index_array = ee.Image.constant([1, 2, 3, 4, 5, 6])\
@@ -71,19 +67,19 @@ def median_absolute_deviation(images, crop_mask, made_scale=2):
     # index_img = index_img.mask(sort_img.neq(-9999))
 
     # # Add mean, ens_meidan, made, upper and lower to ensamble for map display
+    # ens_mean = ensemble_img.reduce(ee.Reducer.mean()).rename(["mean"])
     # ensemble = ensemble_sims_crop.addBands(ens_mean).addBands(ens_median)
     #     .addBands(MADe).addBands(upper).addBands(lower)
     #     .addBands(model_drop_mean)
 
 
 # CGM - Mean ensemble placeholder function
-def mean(images, crop_mask=None):
+def mean(images):
     """
 
     Parameters
     ----------
-    images
-    crop_mask
+    images : list
 
     Returns
     -------
--- openet/core/tests/test_ensemble.py
+++ openet/core/tests/test_ensemble.py
@@ -12,55 +12,106 @@ import openet.core.utils as utils
 
 
 @pytest.mark.parametrize(
-    "model_values, crop_mask, made_scale, expected",
+    "model_values, made_scale, expected",
     [
-        [[118, 111, 57, 75, 99, 58], 1, 2, 86.3333],
-        # CGM - Crop masking isn't currently used
-        [[118, 111, 57, 75, None, 58], 1, 2, 83.8],
-        [[118, 111, 57, 75, 0, 58], 0, 2, 69.8333],
+        [[118, 111, 57, 75, 99, 58], 2, 86.3333],
+        # Check that None values get converted to masked values
+        [[118, 111, 57, 75, None, 58], 2, 83.8],
+        [[118, 111, 57, 75, 0, 58], 2, 69.8333],
+        # Check the edges of the upper threshold
+        [[87, 55, 23, 25, 41, 12], 2, 31.2],
+        [[79, 71, 23, 25, 41, 12], 2, 34.4],
+        [[78, 71, 23, 25, 41, 12], 2, 41.6667],
+        [[77, 71, 23, 25, 40, 12], 2, 34.2],
+        [[76, 71, 23, 25, 40, 12], 2, 41.1667],
+        # Some real values that Thomas found
+        # July 2020 37.36681547, -115.8973587
+        [[0, 0, 1, 15, None, 0], 2, 0.25],
+        # July 2020 36.62667211, -116.4601847
+        [[0, 0, 13, 8, None, 0], 2, 2],
+        # July 2020 37.82405775, -115.8266657
+        [[0, 0, 0, 12, None, 0], 2, 0],
+
+        # Drop model values with SIMS included
+        [[None, 71, 23, 25, 41, 12], 2, 25.25],
+        [[None, None, 23, 25, 41, 12], 2, 25.25],
+        [[None, None, None, 25, 41, 12], 2, 26],
+        [[None, None, None, None, 41, 12], 2, 26.50],
+        [[None, None, None, None, 41, None], 2, 41],
+        # Drop model values with SIMS excluded
+        [[None, 71, 23, 25, None, 12], 2, 32.75],
+        [[None, None, 23, 25, None, 12], 2, 20],
+        [[None, None, None, 25, None, 12], 2, 18.5],
+        [[None, None, None, None, None, 12], 2, 12],
+
+        # Test combinations of masking, zeros, and values
+        [[None, None, None, None, None, 12], 2, 12],
+        [[None, None, None, None, 0, 12], 2, 6],
+        [[None, None, None, 0, 0, 12], 2, 4],
+        [[None, None, 0, 0, 0, 12], 2, 3],
+        [[None, 0, 0, 0, 0, 12], 2, 0],
+        [[0, 0, 0, 0, 0, 12], 2, 0],
+        [[None, None, None, None, 41, 12], 2, 26.5],
+        [[None, None, None, 0, 41, 12], 2, 17.6667],
+        [[None, None, 0, 0, 41, 12], 2, 13.25],
+        [[None, 0, 0, 0, 41, 12], 2, 3],
+        [[0, 0, 0, 0, 41, 12], 2, 0],
+        [[None, None, None, 25, 41, 12], 2, 26],
+        [[None, None, 0, 25, 41, 12], 2, 19.5],
+        [[None, 0, 0, 25, 41, 12], 2, 15.6],
+        [[0, 0, 0, 25, 41, 12], 2, 3],
+        [[None, None, 23, 25, 41, 12], 2, 25.25],
+        [[None, 0, 23, 25, 41, 12], 2, 20.2],
+        [[0, 0, 23, 25, 41, 12], 2, 16.8333],
+        [[None, 55, 23, 25, 41, 12], 2, 31.2],
+        [[0, 55, 23, 25, 41, 12], 2, 26],
+        # # Test various number of only zeros
+        # [[None, None, None, None, None, None], 2, 0],
+        # [[None, None, None, None, None, 0], 2, 0],
+        # [[None, None, None, None, 0, 0], 2, 0],
+        # [[None, None, None, 0, 0, 0], 2, 0],
+        # [[None, None, 0, 0, 0, 0], 2, 0],
+        # [[None, 0, 0, 0, 0, 0], 2, 0],
+        # [[0, 0, 0, 0, 0, 0], 2, 0],
     ]
 )
-def test_mean(model_values, made_scale, crop_mask, expected, tol=0.0001):
-    # input_img = ee.Image.constant(int(img_value, 2)).rename(['BQA'])
-    # input_args = {'input_img': input_img}
+def test_mad(model_values, made_scale, expected, tol=0.001):
+    # TODO: Check if using constant images is faster and works
     images = []
-    for value in model_values:
+    mask_img = ee.Image('IDAHO_EPSCOR/GRIDMET/20200101')\
+        .select(['tmmx']).multiply(0)
+    for i, value in enumerate(model_values):
         if value is None:
-            images.append(ee.Image.constant(0).updateMask(0))
+            images.append(mask_img.updateMask(0).rename([f'B{i+1}']))
         else:
-            images.append(ee.Image.constant(value))
-
-    output_img = ensemble.mean(
-        images=images, crop_mask=ee.Image.constant(crop_mask))
-    output = utils.constant_image_value(output_img)['ensemble']
-    assert abs(output - expected) <= tol
-
-
-@pytest.mark.parametrize(
-    "model_values, crop_mask, made_scale, expected",
-    [
-        [[118, 111, 57, 75, 99, 58], 1, 2, 86.3333],
-        # CGM - Checking if None values get handled as expected
-        [[118, 111, 57, 75, None, 58], 1, 2, 83.8],
-        [[118, 111, 57, 75, 0, 58], 1, 2, 69.83],
-        # [[87, 55, 23, 25, 41, 12], 1, 2, 50],
-    ]
-)
-def test_mad(model_values, made_scale, crop_mask, expected, tol=0.0001):
-    # input_img = ee.Image.constant(int(img_value, 2)).rename(['BQA'])
-    # input_args = {'input_img': input_img}
-    images = []
-    for value in model_values:
-        if value is None:
-            images.append(ee.Image.constant(0).updateMask(0))
-        else:
-            images.append(ee.Image.constant(value))
+            images.append(mask_img.add(value).rename([f'B{i+1}']))
 
     output_img = ensemble.median_absolute_deviation(
-        images=images, crop_mask=ee.Image.constant(crop_mask),
-        made_scale=made_scale)
-    output = utils.constant_image_value(output_img)['ensemble']
+        images=images, made_scale=made_scale)
+    output = utils.point_image_value(output_img, [-120, 39])['ensemble']
     assert abs(output - expected) <= tol
 
 
-
+# @pytest.mark.parametrize(
+#     "model_values, crop_mask, made_scale, expected",
+#     [
+#         [[118, 111, 57, 75, 99, 58], 1, 2, 86.3333],
+#         # CGM - Crop masking isn't currently used
+#         [[118, 111, 57, 75, None, 58], 1, 2, 83.8],
+#         [[118, 111, 57, 75, 0, 58], 0, 2, 69.8333],
+#     ]
+# )
+# def test_mean(model_values, made_scale, crop_mask, expected, tol=0.0001):
+#     # input_img = ee.Image.constant(int(img_value, 2)).rename(['BQA'])
+#     # input_args = {'input_img': input_img}
+#     images = []
+#     for value in model_values:
+#         if value is None:
+#             images.append(ee.Image.constant(0).updateMask(0))
+#         else:
+#             images.append(ee.Image.constant(value))
+#
+#     output_img = ensemble.mean(
+#         images=images, crop_mask=ee.Image.constant(crop_mask))
+#     output = utils.constant_image_value(output_img)['ensemble']
+#     assert abs(output - expected) <= tol
