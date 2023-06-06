71ec27fc6119ec59dd9567333e647b9eab8d8f4b
Peter ReVelle <prevelle@gmail.com>
2023-06-06 13:18:54 -0500

    Removed collection id from being passed directly in test so uses default from function

--- openet/core/tests/test_common.py
+++ openet/core/tests/test_common.py
@@ -263,8 +263,7 @@ def test_landsat_c2_sr_lst_parameters():
     sr_img = ee.Image('LANDSAT/LC08/C02/T1_L2/LC08_030036_20210725')
     ndvi_img = sr_img.multiply(0.0000275).add(-0.2).normalizedDifference(['SR_B5', 'SR_B4'])
     output_img = common.landsat_c2_sr_lst_correct(
-        sr_image=sr_img, ndvi=ndvi_img,
-        soil_emis_coll_id='projects/earthengine-legacy/assets/projects/openet/soil_emissivity/tiles',
+        sr_image=sr_img, ndvi=ndvi_img
     )
     output = utils.get_info(output_img)
     assert output['bands'][0]['id'] == 'surface_temperature'
