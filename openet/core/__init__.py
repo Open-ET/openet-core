61fc9ad52be576a58fbb2cd61d16cdcd93226569
Charles Morton <cgmorton@users.noreply.github.com>
2023-09-20 14:14:21 -0700

    Update __version__ in init
    The version number wasn't being pulled from the pyproject.toml since the package name has a period (i.e. openet.core) but the name in the pyproject.toml is with a hyphen (i.e. openet-core).  This is probably not the best approach but it seems work

--- openet/core/__init__.py
+++ openet/core/__init__.py
@@ -5,9 +5,10 @@ from . import interpolate
 from . import utils
 from . import wrs2
 
-# try:
-#     from importlib import metadata
-# except ImportError:  # for Python<3.8
-#     import importlib_metadata as metadata
+try:
+    from importlib import metadata
+except ImportError:  # for Python<3.8
+    import importlib_metadata as metadata
 
-# __version__ = metadata.version(__package__ or __name__)
+__version__ = metadata.version(__package__.replace('.', '-') or __name__.replace('.', '-'))
+# __version__ = metadata.version('openet-core')
