e5e32511aaf2e546c79439ba6043c151b7ad0c9a
Charles Morton <cgmorton@users.noreply.github.com>
2023-11-10 06:57:05 -0800

    Update ensemble.py
    Formatting and a couple of type fixes

--- openet/core/ensemble.py
+++ openet/core/ensemble.py
@@ -43,36 +43,42 @@ def mad(ensemble_img, made_scale=2):
     MADe = ensemble_img.subtract(ens_median).abs()\
         .reduce(ee.Reducer.median()).multiply(1.4826)
 
-    # Calulate upper and lower bounds for outlier, based on median
+    # Calculate upper and lower bounds for outlier, based on median
     upper = ens_median.add(MADe.multiply(made_scale)).rename(["upper"])
     lower = ens_median.subtract(MADe.multiply(made_scale)).rename(["lower"])
 
     # Count of ensemble values after applying MAD filtering
     # Note, this is not the final count of models used in the ensemble
-    mad_count = ensemble_img.gte(lower).And(ensemble_img.lte(upper))\
-        .reduce(ee.Reducer.sum())\
-        .clamp(min_model_count, model_count)\
+    mad_count = (
+        ensemble_img.gte(lower).And(ensemble_img.lte(upper))
+        .reduce(ee.Reducer.sum())
+        .clamp(min_model_count, model_count)
         .rename(["count"])
+    )
 
     ens_array = ensemble_img.unmask(-9999).toArray()
     diff_array = ens_array.subtract(ens_median)
 
-    mad_img = ens_array.arraySort(diff_array.abs())\
-        .arraySlice(0, 0, mad_count)\
-        .arrayCat(ee.Array(-9999).repeat(0, model_count), 0)\
-        .arraySlice(0, 0, model_count)\
+    mad_img = (
+        ens_array.arraySort(diff_array.abs())
+        .arraySlice(0, 0, mad_count)
+        .arrayCat(ee.Array(-9999).repeat(0, model_count), 0)
+        .arraySlice(0, 0, model_count)
         .arrayFlatten([output_bands])
+    )
     mad_img = mad_img.mask(mad_img.neq(-9999))
 
     # TODO: Add a flag or parameter for returning additional bands
     # TODO: Move the model bit image
-    output_img = mad_img\
+    output_img = (
+        mad_img
         .reduce(ee.Reducer.mean()
                 .combine(ee.Reducer.min(), sharedInputs=True)
                 .combine(ee.Reducer.max(), sharedInputs=True)
                 .combine(ee.Reducer.count(), sharedInputs=True))\
         .rename(["ensemble_mad", "ensemble_mad_min", "ensemble_mad_max",
-                 "ensemble_mad_count"])\
+                 "ensemble_mad_count"])
+    )
 
     # Map the band names to the model index
     # The extra combine is to try and account for the ensemble images having
@@ -86,14 +92,17 @@ def mad(ensemble_img, made_scale=2):
     # Bit encode the models using the model index values
     # Build the index array from the ensemble images so that the index
     #   is masked the same
-    index_array = ensemble_img\
-        .multiply(0).add(ee.Image.constant(band_index))\
+    index_array = (
+        ensemble_img.multiply(0).add(ee.Image.constant(band_index))
         .unmask(-9999).toArray()
-    index_img = index_array.arraySort(diff_array.abs())\
-        .arraySlice(0, 0, mad_count)\
-        .arrayCat(ee.Array(-9999).repeat(0, model_count), 0)\
-        .arraySlice(0, 0, model_count)\
+    )
+    index_img = (
+        index_array.arraySort(diff_array.abs())
+        .arraySlice(0, 0, mad_count)
+        .arrayCat(ee.Array(-9999).repeat(0, model_count), 0)
+        .arraySlice(0, 0, model_count)
         .arrayFlatten([output_bands])
+    )
     index_img = index_img.mask(index_img.neq(-9999))
     index_img = index_img.multiply(0).add(2).pow(index_img.subtract(1))\
         .reduce(ee.Reducer.sum()).int().rename(['ensemble_mad_index'])
@@ -111,7 +120,7 @@ def mad(ensemble_img, made_scale=2):
     # print(utils.point_image_value(sort_img, [-120, 39], scale=1))
     # print(utils.point_image_value(model_drop_mean, [-120, 39], scale=1))
 
-    # # Add mean, ens_meidan, made, upper and lower to ensamble for map display
+    # # Add mean, ens_median, made, upper and lower to ensemble for map display
     # ens_mean = ensemble_img.reduce(ee.Reducer.mean()).rename(["mean"])
     # ensemble = ensemble_sims_crop.addBands(ens_mean).addBands(ens_median)
     #     .addBands(MADe).addBands(upper).addBands(lower)
