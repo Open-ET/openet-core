2ca5316453a98fc906340aaa9bc6f9a4e6621a2f
Charles Morton <cgmorton@users.noreply.github.com>
2024-02-13 05:59:24 -0800

    Bumped minimum python version to 3.8

--- openet/core/__init__.py
+++ openet/core/__init__.py
@@ -5,10 +5,7 @@ from . import interpolate
 from . import utils
 from . import wrs2
 
-try:
-    from importlib import metadata
-except ImportError:  # for Python<3.8
-    import importlib_metadata as metadata
+from importlib import metadata
 
 __version__ = metadata.version(__package__.replace('.', '-') or __name__.replace('.', '-'))
 # __version__ = metadata.version('openet-core')
