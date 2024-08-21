b75db98dc046c93e7fd434153ec915757856602d
Charles Morton <cgmorton@users.noreply.github.com>
2024-08-21 10:17:26 -0700

    Testing WIF authentication

--- openet/core/tests/conftest.py
+++ openet/core/tests/conftest.py
@@ -13,13 +13,13 @@ def test_init():
     logging.getLogger('googleapiclient').setLevel(logging.ERROR)
     logging.debug('Test Setup')
 
-    # For GitHub Actions authenticate using private key environment variable
-    if "ACTION_EE_TOKEN" in os.environ:
-        # ACTION_EE_TOKEN = os.getenv('ACTION_EE_TOKEN')
-        # credentials = google.oauth2.credentials.Credentials(ACTION_EE_TOKEN)
-        # ee.Initialize(credentials)
-        ee.Initialize(google.oauth2.credentials.Credentials(os.getenv('ACTION_EE_TOKEN')))
-    elif 'EE_PRIVATE_KEY_B64' in os.environ:
+    # # For GitHub Actions authenticate using private key environment variable
+    # if "ACTION_EE_TOKEN" in os.environ:
+    #     # ACTION_EE_TOKEN = os.getenv('ACTION_EE_TOKEN')
+    #     # credentials = google.oauth2.credentials.Credentials(ACTION_EE_TOKEN)
+    #     # ee.Initialize(credentials)
+    #     ee.Initialize(google.oauth2.credentials.Credentials(os.getenv('ACTION_EE_TOKEN')))
+    if 'EE_PRIVATE_KEY_B64' in os.environ:
         print('Writing privatekey.json from environmental variable ...')
         content = base64.b64decode(os.environ['EE_PRIVATE_KEY_B64']).decode('ascii')
         EE_KEY_FILE = 'privatekey.json'
